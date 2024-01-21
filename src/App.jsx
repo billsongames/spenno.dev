0d9b62e1b0e41a8cc9a20da7dea19c5a51de5883
Paul Spencer <billsongames@gmail.com>
2024-01-21 00:43:30 +0000

    holding pages and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import './App.css';
 
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+
 
 const App = () => {
 
@@ -16,7 +19,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path = "/" element = {<Home/>} />
+          <Route path = "/about" element = {<About/>} />
           <Route path = "/projects" element = {<Projects/>} />
+          <Route path = "/contact" element = {<Contact/>} />
         </Routes>    
       </div>
     </BrowserRouter>
