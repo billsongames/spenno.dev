26e2c2c30a87c3ae5484a4b7af4d428113821c8a
billsongames <billsongames@gmail.com>
2024-01-26 00:02:14 +0000

    about section started

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import "./about.css"
+
 const About = () => {
   return(
-    <div>
-      About
+    <div className="about__container">
+      <div className="about__text">
+         Hi. I'm Paul Spencer, a software developer based in Doncaster, UK. I enjoy building interactive and intuitive user experiences.
+      </div>
+      <div className="about__text">
+        I also have proficiency developing video games with two full releases on Android and PC platforms.
+      </div>
+      <div className="about__text">
+        I also have a strong enthusiasm for anything to do with the 1980s, particularly retro game related stuff and rock music, which is probably why I enjoy playing video games and the electric guitar, although not necessarily at the same time, unless it's a quick blast on Guitar Hero.
+      </div>
     </div>
   )
 }
