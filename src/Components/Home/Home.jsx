c008cc2ef8774688f3204baee4e5d2c7dae188be
Paul Spencer <billsongames@gmail.com>
2024-02-17 22:18:27 +0000

    home about added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,6 +18,20 @@ const Home = () => {
         </div>
         <HomeGallery />
       </div>
+      <div className="home-about-container">
+        <div>
+          <img src="/assets/images/profile/ps.jpeg" className="about-intro-profilePic"/>
+        </div>
+        <div className="home-about-text">
+          <div class>
+            Hi. I'm Paul Spencer.
+          </div>
+          <div>
+            I enjoy building interactive and intuitive user experiences.
+          </div>
+
+        </div>
+      </div>
       <Contact />
     </div>
   )
